9b92399b423dc150cdb4878decdd29fc373c9e4a
Ionut-Mihai Sandu <rowebsoft@gmail.com>
2017-07-01 11:32:39 +0200

    added dirname to fileget

--- index.php
+++ index.php
@@ -10,15 +10,16 @@ if ($resourceDomain && $resourceType && $resourceName) {
 
     if (in_array($resourceDomain, Config::$domains) && in_array($resourceType, Config::$type)) {
         $dir = $resourceDomain . DIRECTORY_SEPARATOR . $resourceType . DIRECTORY_SEPARATOR;
-        if ($file = is_file($dir . $resourceName . '.' . $resourceType)) {
+        if ($file = is_file(dirname(__FILE__) . $dir . $resourceName . '.' . $resourceType)) {
             header("Content-type: text/css", true);
             header("Content-Length: " . filesize($file));
             readfile($file);
             exit();
         } else {
             if ($debug == 1) {
-                throw new \Exception(' file :: ' . var_export(is_file($dir . $resourceName . '.' . $resourceType))
-                    . ' file::' . $dir . $resourceName . '.' . $resourceType
+                throw new \Exception(' file :: '
+                    . var_export(is_file(dirname(__FILE__) . $dir . $resourceName . '.' . $resourceType))
+                    . ' file::' . dirname(__FILE__) . $dir . $resourceName . '.' . $resourceType
                 );
             }
         }
