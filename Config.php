f72c82a89a4731bc7549792a8ca8b46fa6409c6d
Ionut-Mihai Sandu <rowebsoft@gmail.com>
2017-07-01 11:01:15 +0200

    added config and file  displayment

---
+++ Config.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Mike
+ * Date: 7/1/2017
+ * Time: 10:41 AM
+ */
+
+class Config
+{
+    const DOMAINS = [
+        'autoshack.ro'
+    ];
+
+    const TYPE = [
+        'js',
+        'css'
+    ];
+}
--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
 //hello world
-echo "Hello World!";
+$resourcedomain = filter_input(INPUT_GET, 'domain', FILTER_SANITIZE_STRING);
+$resourceType = filter_input(INPUT_GET, 'type', FILTER_SANITIZE_STRING);
+$resourceName = filter_input(INPUT_GET, 'name', FILTER_SANITIZE_STRING);
+require_once('Config.php');
+
+if ($resourcedomain && $resourceType && $resourceName) {
+
+    if (in_array($resourcedomain, Config::DOMAINS) && in_array($resourceType, Config::TYPE))
+    {
+        $dir = $resourcedomain.DIRECTORY_SEPARATOR.$resourceType;
+        if( $file = is_file($dir.$resourceName.$resourceType)){
+            header("Content-type: text/css", true);
+            header("Content-Length: " . filesize($file));
+            readfile($file);
+            exit();
+        }
+    }
+}
+throw new \Exception('Hello world, this is a private service please don\'t use it!');
