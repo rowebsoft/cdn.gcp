6620f346d5f00bbbccb7a4ef774f3fdb83d5005e
Ionut-Mihai Sandu <rowebsoft@gmail.com>
2017-07-01 11:16:12 +0200

    added debug and else cond

--- index.php
+++ index.php
@@ -1,21 +1,34 @@
 <?php
 //hello world
-$resourcedomain = filter_input(INPUT_GET, 'domain', FILTER_SANITIZE_STRING);
+$resourceDomain = filter_input(INPUT_GET, 'domain', FILTER_SANITIZE_STRING);
 $resourceType = filter_input(INPUT_GET, 'type', FILTER_SANITIZE_STRING);
 $resourceName = filter_input(INPUT_GET, 'name', FILTER_SANITIZE_STRING);
+$debug = filter_input(INPUT_GET, 'dbg', FILTER_SANITIZE_STRING);
 require_once('Config.php');
 
-if ($resourcedomain && $resourceType && $resourceName) {
+if ($resourceDomain && $resourceType && $resourceName) {
 
-    if (in_array($resourcedomain, Config::DOMAINS) && in_array($resourceType, Config::TYPE))
-    {
-        $dir = $resourcedomain.DIRECTORY_SEPARATOR.$resourceType;
-        if( $file = is_file($dir.$resourceName.$resourceType)){
+    if (in_array($resourceDomain, Config::DOMAINS) && in_array($resourceType, Config::TYPE)) {
+        $dir = $resourceDomain . DIRECTORY_SEPARATOR . $resourceType;
+        if ($file = is_file($dir . $resourceName . '.' . $resourceType)) {
             header("Content-type: text/css", true);
             header("Content-Length: " . filesize($file));
             readfile($file);
             exit();
+        } else {
+            if ($debug == 1) {
+                throw new \Exception(' file :: ' . var_export($file) . ' fir::' . $dir);
+            }
+        }
+    } else {
+        if ($debug == 1) {
+            throw new \Exception(
+                'Not in array or not in config type cond 1::'
+                . var_export(in_array($resourceDomain, Config::DOMAINS)) . 'cond 2::'
+                . var_export(in_array($resourceType, Config::TYPE))
+            );
         }
     }
+} else {
+    throw new \Exception('Hello world, this is a private service please don\'t use it!');
 }
-throw new \Exception('Hello world, this is a private service please don\'t use it!');
